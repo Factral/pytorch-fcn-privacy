edb391fb42b56b20c18185008bba74554650d7d3
Fabian *-* <nelsonfabiancs8@gmail.com>
2023-08-22 17:15:53 -0500

    torch no grad

--- examples/voc/evaluate.py
+++ examples/voc/evaluate.py
@@ -67,38 +67,41 @@ def main():
     print('==> Evaluating with VOC2011ClassSeg seg11valid')
     visualizations = []
     label_trues, label_preds = [], []
-    for batch_idx, (data, target) in tqdm.tqdm(enumerate(val_loader),
-                                               total=len(val_loader),
-                                               ncols=80, leave=False):
-        if torch.cuda.is_available():
-            data, target = data.cuda(), target.cuda()
-        data, target = Variable(data, volatile=True), Variable(target)
-        score = model(data)
 
-        imgs = data.data.cpu()
-        lbl_pred = score.data.max(1)[1].cpu().numpy()[:, :, :]
-        lbl_true = target.data.cpu()
-        for img, lt, lp in zip(imgs, lbl_true, lbl_pred):
-            img, lt = val_loader.dataset.untransform(img, lt)
-            label_trues.append(lt)
-            label_preds.append(lp)
-            if len(visualizations) < 9:
-                viz = fcn.utils.visualize_segmentation(
-                    lbl_pred=lp, lbl_true=lt, img=img, n_class=n_class,
-                    label_names=val_loader.dataset.class_names)
-                visualizations.append(viz)
-    metrics = torchfcn.utils.label_accuracy_score(
-        label_trues, label_preds, n_class=n_class)
-    metrics = np.array(metrics)
-    metrics *= 100
-    print('''\
-Accuracy: {0}
-Accuracy Class: {1}
-Mean IU: {2}
-FWAV Accuracy: {3}'''.format(*metrics))
+    with torch.no_grad():
+        for batch_idx, (data, target) in tqdm.tqdm(enumerate(val_loader),
+                                                total=len(val_loader),
+                                                ncols=80, leave=False):
+            if torch.cuda.is_available():
+                data, target = data.cuda(), target.cuda()
+            
+            #data, target = Variable(data, volatile=True), Variable(target)
+            score = model(data)
 
-    viz = fcn.utils.get_tile_image(visualizations)
-    skimage.io.imsave('viz_evaluate.png', viz)
+            imgs = data.data.cpu()
+            lbl_pred = score.data.max(1)[1].cpu().numpy()[:, :, :]
+            lbl_true = target.data.cpu()
+            for img, lt, lp in zip(imgs, lbl_true, lbl_pred):
+                img, lt = val_loader.dataset.untransform(img, lt)
+                label_trues.append(lt)
+                label_preds.append(lp)
+                if len(visualizations) < 9:
+                    viz = fcn.utils.visualize_segmentation(
+                        lbl_pred=lp, lbl_true=lt, img=img, n_class=n_class,
+                        label_names=val_loader.dataset.class_names)
+                    visualizations.append(viz)
+        metrics = torchfcn.utils.label_accuracy_score(
+            label_trues, label_preds, n_class=n_class)
+        metrics = np.array(metrics)
+        metrics *= 100
+        print('''\
+    Accuracy: {0}
+    Accuracy Class: {1}
+    Mean IU: {2}
+    FWAV Accuracy: {3}'''.format(*metrics))
+
+        viz = fcn.utils.get_tile_image(visualizations)
+        skimage.io.imsave('viz_evaluate.png', viz)
 
 
 if __name__ == '__main__':
